eb71167308bfd074408eb378b9466a8637bfac02
Eric Guzman <eguzman@smu.edu>
2018-07-30 03:05:17 -0500

    Remove a data set

--- plottempdata.py
+++ plottempdata.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 # This program reads in a csv file of temp data and creates a plot.
-# ./plottempdata.py kanktempdata kaittempdata valentitempdata outfilename
+# Usage:
+# ./plottempdata.py kaittempdata konktempdata outfilename
 
 import sys
 import csv
@@ -22,18 +23,16 @@ def gettempdata(filename):
 
 mjd, temp, temperr = gettempdata(sys.argv[1])
 mjd2, temp2, temperr2 = gettempdata(sys.argv[2])
-mjd3, temp3, temperr3 = gettempdata(sys.argv[3])
 
 # Plotting
 plt.figure()
-plt.errorbar(np.array(mjd2) - 56496.9, np.array(temp2)/1000, yerr=np.array(temperr2)/1000, label='KAIT', fmt='o', capsize=2, c='lawngreen')
-plt.errorbar(np.array(mjd) - 56496.9, np.array(temp)/1000, yerr=np.array(temperr)/1000, label='Konkoly', fmt='o', capsize=2, c='blue')
-plt.errorbar(np.array(mjd3) - 56496.9, np.array(temp3)/1000, yerr=np.array(temperr3)/1000, label='Valenti et. al., (2014)', fmt='o', capsize=2, c='red')
+plt.errorbar(np.array(mjd) - 56496.9, np.array(temp)/1000, yerr=np.array(temperr)/1000, label='KAIT', fmt='o', capsize=2, c='lawngreen')
+plt.errorbar(np.array(mjd2) - 56496.9, np.array(temp2)/1000, yerr=np.array(temperr2)/1000, label='Konkoly', fmt='o', capsize=2, c='blue')
 plt.xlabel("MJD-56496.9")
 plt.ylabel("Temperature ($10^{3}$ K)")
 plt.title("SN2013ej Temperature")
 plt.legend()
 plt.ylim(2,16)
 plt.xlim(0,110)
-plt.savefig(str(sys.argv[4]))
+plt.savefig(str(sys.argv[3]))
 plt.close()
